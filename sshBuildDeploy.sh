19dbc3cbc521a4873832a79b7b61c68c737cd54e
Xiaoning He <xh1019@ic.ac.uk>
2021-04-01 11:59:11 +0100

    Use shell script

--- sshBuildDeploy.sh
+++ sshBuildDeploy.sh
@@ -1,5 +1,1 @@
-# if connection output contains success then:
-cd template-ros-core
-git checkout deployment
-git pull
 bash -c "./buildAndDeploy.sh autobot06 indefinite_navigation duckietown_demos duckietown/template-ros-core:demo_24_feb-arm32v7"
