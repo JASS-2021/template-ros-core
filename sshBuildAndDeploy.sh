e38a0926bc371944c0e6cd4fee046f6cde24b6d8
Guolin <gy119@ic.ac.uk>
2021-04-01 15:33:12 +0100

    deploy yml file updated with new build and deploy script

--- sshBuildAndDeploy.sh
+++ sshBuildAndDeploy.sh
@@ -7,6 +7,9 @@
 #   buildAndDeploy script in same directory as script
 #   git installed on machine we ssh into
 
+demo_name=$1
+package_name=$2
+image_name=$3
     
 ssh -i ssh_key jass@5.19.248.97 -p 2222 << EOF
     declare -a fleet=(autobot06 autobot10)
@@ -16,6 +19,6 @@ ssh -i ssh_key jass@5.19.248.97 -p 2222 << EOF
     for bot in \${fleet[@]}; do
         echo \$bot
         dts devel build -f -H \$bot.local
-        dts duckiebot demo --demo_name circle_drive --duckiebot_name \$bot --package_name circle_drive --image duckietown/template-ros-core:deployment-arm32v7 --debug
+        dts duckiebot demo --demo_name $1 --duckiebot_name \$bot --package_name $2 --image $3
     done
 EOF
