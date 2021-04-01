6a27ee99c8951592be46746e124a0833314532dc
Guolin <gy119@ic.ac.uk>
2021-04-01 12:37:03 +0100

    ssh build and deploy script added

---
+++ sshBuildAndDeploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# pre req: 
+#   template-ros-core repo is already cloned
+#   vpn jass-spb already configured
+#   ssh_key file in same directory as script
+#   buildAndDeploy script in same directory as script
+#   git installed on machine we ssh into
+
+    
+ssh -i ssh_key jass@5.19.248.97 -p 2222 << EOF
+    declare -a fleet=(autobot06 autobot10)
+    cd template-ros-core
+    ls
+    git pull
+    for bot in \${fleet[@]}; do
+        echo \$bot
+        dts devel build -f -H \$bot.local
+        dts duckiebot demo --demo_name circle_drive --duckiebot_name \$bot --package_name circle_drive --image duckietown/template-ros-core:deployment-arm32v7 --debug
+    done
+EOF
