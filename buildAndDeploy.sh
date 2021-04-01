ba2c68f268b93263ecccd281cf14e61af0e0ef3a
Xiaoning He <xh1019@ic.ac.uk>
2021-04-01 11:33:10 +0100

    Changed to autobot06

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -5,11 +5,9 @@ demo_name=$2
 package_name=$3
 image_name=$4
 
-echo $duckie_bot $demo_name $package_name $image_name
-
 dts devel build -f -H $duckie_bot.local
 
 dts duckiebot demo --demo_name $demo_name --duckiebot_name $duckie_bot --package_name $package_name --image $image_name --debug
 
 # Example script to run build and deploy in detached screen terminal 
-# screen -d -m -L -S "circle_drive" bash -c "./buildAndDeploy.sh autobotpi4 circle_drive circle_drive duckietown/template-ros-core:demo_24_feb-arm32v7"
+# bash -c "./buildAndDeploy.sh autobot06 indefinite_navigation duckietown_demos duckietown/template-ros-core:demo_24_feb-arm32v7"
